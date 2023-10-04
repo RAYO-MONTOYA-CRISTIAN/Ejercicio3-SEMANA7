85b43f69ab7bb87fb9ad2e7f94865cdc087258ae
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-10-03 22:59:20 -0500

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Años bisiestos desde 1900 hasta la actualdiad: ");
+
+            for (int año = 1900; año <= DateTime.Now.Year; año++)
+            {
+                if (año % 4 == 0 && (año % 100 != 0 || año % 400 == 0))
+                {
+                    Console.WriteLine(año);
+                }
+
+
+
+            }    
+        }
+    }
+}
